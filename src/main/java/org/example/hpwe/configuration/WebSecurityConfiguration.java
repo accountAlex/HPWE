d1e83aaf9cfbb0f801bbdec979dadae02b62d17d
alexanderzarechkin <alexander.zarechkin@yandex.ru>
2024-03-25 14:17:44 +0300

    first test commit

--- src/main/java/org/example/hpwe/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/example/hpwe/configuration/WebSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
+    //comment
     @Bean
     public SecurityFilterChain securityConfig(
             HttpSecurity http
